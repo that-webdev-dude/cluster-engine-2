a8a6bb9faf9ab2bb0ea700b74a8d09dd76217599
that.webdev.dude <that.webdev.dude@gmail.com>
2024-11-24 17:21:28 +0000

    store patch

--- src/cluster/index.ts
+++ src/cluster/index.ts
@@ -3,11 +3,11 @@ export { Component } from "./core/ECS";
 export { System } from "./core/ECS";
 export { Scene } from "./core/ECS";
 export { Game } from "./core/ECS";
+export { Store } from "./core/Store";
 
 export { Mouse } from "./core/Input";
 export { Keyboard } from "./core/Input";
 
-export { Emitter } from "./core/Emitter";
 export { Event } from "./core/Emitter";
 
 export { Cmath } from "./tools/Cmath";
