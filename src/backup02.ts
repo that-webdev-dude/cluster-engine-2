4dc7d3db5234b70bd425d80aebdf69a790eeab4d
that.webdev.dude <that.webdev.dude@gmail.com>
2024-12-14 10:20:16 +0000

    bo

--- src/backup02.ts
+++ src/backup02.ts
@@ -1,15 +1,8 @@
 import { Display } from "./cluster/core/Display";
-import { Engine } from "./cluster/core/Engine";
-import { Assets } from "./cluster/core/Assets";
-import { Store } from "./cluster/core/Store";
-import { Mouse } from "./cluster/core/Input";
-import { Keyboard } from "./cluster/core/Input";
 
 type ComponentType = string;
 
 type ComponentIndex = number;
-
-type EntityId = number;
 
 /** ComponentStore
  * a class that holds a dictionary of component types and indices.
@@ -17,9 +10,7 @@ type EntityId = number;
 class ComponentStore {
   private static _nextIndex: number = 1;
 
-  static dictionary: Map<ComponentType, ComponentIndex> = new Map();
-
-  static storage: Map<ComponentIndex, Map<EntityId, Component>> = new Map();
+  static readonly dictionary: Map<ComponentType, ComponentIndex> = new Map();
 
   static register(type: ComponentType): ComponentIndex {
     if (!ComponentStore.dictionary.has(type)) {
@@ -27,37 +18,19 @@ class ComponentStore {
     }
     return ComponentStore.dictionary.get(type)!;
   }
-
-  static addComponent(entityId: EntityId, component: Component): void {
-    const index = component.index;
-    if (!this.storage.has(index)) {
-      this.storage.set(index, new Map());
-    }
-    this.storage.get(index)!.set(entityId, component);
-  }
-
-  static getComponent<T>(
-    entityId: EntityId,
-    index: ComponentIndex
-  ): T | undefined {
-    return this.storage.get(index)?.get(entityId) as T | undefined;
-  }
-
-  static removeComponent(entityId: EntityId, index: ComponentIndex): void {
-    this.storage.get(index)?.delete(entityId);
-  }
 }
 
 /** Entity
  * an ID that holds a set of components and children entities.
  */
 class Entity {
-  private static _nextId: EntityId = 1;
-  private _id: EntityId;
+  private static _nextId: number = 1;
+  private _id: number;
   private _mask: bigint;
   private _type: string;
   public dead: boolean;
   public children: Set<Entity>;
+  public components: Map<ComponentType, Component>;
 
   constructor(type: string) {
     this._id = Entity._nextId++;
@@ -65,15 +38,14 @@ class Entity {
     this._type = type;
     this.dead = false;
     this.children = new Set();
+    this.components = new Map();
   }
 
   private _updateMask(): void {
     this._mask = BigInt(0);
-    for (const [index, storage] of ComponentStore.storage) {
-      if (storage.has(this._id)) {
-        this._mask |= BigInt(1) << BigInt(index);
-      }
-    }
+    this.components.forEach((component) => {
+      this._mask |= BigInt(1) << BigInt(component.index);
+    });
   }
 
   public get id(): number {
@@ -92,38 +64,24 @@ class Entity {
     return this.children.size > 0;
   }
 
-  // here
-  public addComponent(component: Component): Entity {
-    ComponentStore.addComponent(this._id, component);
-    this._updateMask();
-    return this;
-  }
-
-  public removeComponent(type: ComponentType): Entity {
-    const index = ComponentStore.dictionary.get(type)!;
-    ComponentStore.removeComponent(this._id, index);
-    this._updateMask();
-    return this;
-  }
-  // here
-
   public addComponents(...components: Component[]): Entity {
     for (const component of components) {
-      this.addComponent(component);
+      this.components.set(component.type, component);
     }
+    this._updateMask();
     return this;
   }
 
-  public removeComponents(...components: Component[]): Entity {
+  public deleteComponents(...components: Component[]): Entity {
     for (const component of components) {
-      this.removeComponent(component.type);
+      this.components.delete(component.type);
     }
+    this._updateMask();
     return this;
   }
 
   public getComponent<T>(type: ComponentType): T | undefined {
-    const index = ComponentStore.dictionary.get(type)!;
-    return ComponentStore.getComponent<T>(this._id, index);
+    return this.components.get(type) as T;
   }
 
   public addChildren(...entities: Entity[]): Entity {
@@ -163,7 +121,7 @@ abstract class Component {
 }
 
 /** System
- *  a class that updates entities.
+ * a class that updates entities.
  */
 abstract class System {
   private _mask: bigint;
@@ -193,7 +151,7 @@ abstract class System {
 }
 
 /** Scene
- *  a class that holds a set of systems and entity containers (layers).
+ * a class that holds a set of systems and entity containers (layers).
  */
 class Scene {
   readonly entities: Set<Entity>;
@@ -227,83 +185,6 @@ class Scene {
         }
       });
     });
-  }
-}
-
-/** Game
- * a class that holds the current and next scene.
- */
-class Game {
-  private _currentScene: Scene | null;
-  private _nextScene: Scene | null;
-  private _switching: boolean = false;
-  private _emitter: Store;
-
-  constructor(emitter: Store) {
-    this._currentScene = null;
-    this._nextScene = null;
-    this._switching = false;
-    this._emitter = emitter;
-    Mouse.element = Display.view;
-  }
-
-  public get currentScene(): Scene | null {
-    return this._currentScene;
-  }
-
-  public get nextScene(): Scene | null {
-    return this._nextScene;
-  }
-
-  public setScene(scene: Scene): void {
-    if (!this._currentScene) {
-      this._currentScene = scene;
-      return;
-    }
-    if (this._currentScene && !this._nextScene && !this._switching) {
-      this._nextScene = scene;
-      this._switching = true;
-    }
-  }
-
-  public start(callback: (dt: number, t: number) => void): void {
-    Engine.update = (dt: number, t: number) => {
-      Display.clear();
-
-      // Update the current scene
-      if (this._currentScene) {
-        this._currentScene.update(dt, t);
-      }
-
-      // If switching, also update the next scene
-      if (this._switching && this._nextScene) {
-        this._nextScene.update(dt, t);
-
-        // Check if the current scene is dead
-        if (this._currentScene?.dead) {
-          // Swap scenes
-          this._currentScene = this._nextScene;
-          this._nextScene = null;
-          this._switching = false; // Reset the switching flag
-        }
-      }
-
-      callback(dt, t);
-
-      this._emitter.processEvents();
-
-      Keyboard.update();
-      Mouse.update();
-    };
-
-    Assets.onReady(() => {
-      Engine.start();
-    });
-  }
-
-  public stop(): void {
-    this._emitter.clear();
-    Engine.stop(); // ... maybe some form of cleanup function here
   }
 }
 
